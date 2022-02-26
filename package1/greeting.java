fab87ca0959d7d1ddffa8c5c79524eab49f566aa
kumarankitgupta <ankitkrgupta149@gmail.com>
2022-02-26 16:11:18 +0530

    packages in java

---
+++ package1/greeting.java
@@ -0,0 +1,10 @@
+package package1;
+public class greeting {
+    String name;
+
+    public greeting(String n){
+        this.name = n;
+        System.out.println("Hello ' " +name);
+    }
+    
+}
---
+++ packageExample.java
@@ -0,0 +1,6 @@
+import package1.*;
+public class packageExample {
+    public static void main(String[] args) {
+        greeting ob = new greeting("Ankit");
+    }
+}
